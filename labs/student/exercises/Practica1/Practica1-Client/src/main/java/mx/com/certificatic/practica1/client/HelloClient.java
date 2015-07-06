22d6793c8fc8660f3f95d15e829e8eb9e43fce90
sidlors <sidlors@gmail.com>
2015-07-05 22:48:52 -0500

    Client Practica1

---
+++ labs/student/exercises/Practica1/Practica1-Client/src/main/java/mx/com/certificatic/practica1/client/HelloClient.java
@@ -0,0 +1,41 @@
+package mx.com.certificatic.practica1.client;
+
+import javax.xml.ws.WebServiceRef;
+
+import mx.com.certificatic.practica1.service.SayHello;
+import mx.com.certificatic.practica1.service.SayHelloService;
+
+
+public class HelloClient {
+	@WebServiceRef(wsdlLocation = "http://localhost:8080/helloservice/hello?wsdl")
+	static SayHelloService service;
+
+	public static void main(String[] args) {
+		try {
+			HelloClient client = new HelloClient();
+			client.doTest(args);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void doTest(String[] args) {
+		try {
+			System.out
+					.println("Retrieving the port from the following service: "
+							+ service);
+			SayHello port = service.getSayHelloPort();
+			System.out.println("Invoking the sayHello operation on the port.");
+			String name;
+			if (args.length > 0) {
+				name = args[0];
+			} else {
+				name = "No Name";
+			}
+			String response = port.sayHello(name);
+			System.out.println(response);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ labs/student/exercises/Practica1/Practica1-Client/src/main/java/mx/com/certificatic/practica1/service/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://service.practica1.certificatic.com.mx/")
+package mx.com.certificatic.practica1.service;
